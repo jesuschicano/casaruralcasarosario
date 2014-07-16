17a4f003910c494a5f853ba38c2734138f7f18c1
jesus <rurouni.kenshin3988@gmail.com>
2014-07-16 11:18:49 +0200

    added index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Casa Rosario</title>
+</head>
+<body>
+	<div id="wrapper">
+		<header>
+			<div id="portada">
+				<h1>casa rural 'casa rosario'</h1>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="">title1</a></li>
+					<li><a href="">title2</a></li>
+					<li><a href="">title3</a></li>
+					<li><a href="">title4</a></li>
+					<li><a href="">title5</a></li>
+				</ul>
+			</nav>
+			<div id="logueo">
+				<?php
+					// llamada a la función de logueo
+					// sino hay usuario: muestra formulario. Si lo hay: muestra 'hola felipe'
+					//
+
+				?>
+			</div>
+		</header>
+
+		<section>
+			<div><img src="" alt=""></div>
+			<div>
+				<h2>Una forma diferente de acercarse al corazón de Andalucía</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla laborum repellat molestiae quibusdam in perferendis, itaque officia iure a perspiciatis consequuntur error consectetur sunt voluptas, velit magnam quaerat, vel odio!</p>
+			</div>
+		</section>
+		
+		<footer>
+			<p>All rights reserved. &copy;Jesús Chicano <img src="" alt="twitter"><a href=""></a></img>. Developed in 2014. Spain.</p>
+		</footer>
+	</div>
+</body>
+</html>
